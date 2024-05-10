5c5a950229114a842aed1041d96039fea17044ae
Justin Lee <smsc19970205@gmail.com>
2024-05-10 22:32:18 +1000

    EDA update

--- layoff_EDA.sql
+++ layoff_EDA.sql
@@ -5,5 +5,9 @@ USE layoff_dbms;
 SELECT *
 FROM layoffs_staging2;
 
-SELECT * 
-FROM layoffs_staging;
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1;
